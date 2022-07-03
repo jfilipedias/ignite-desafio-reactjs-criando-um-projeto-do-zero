3eb1bf347cbf099a5c4740a2a03adecc73718077
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-03 16:48:26 -0300

    Implements the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,26 @@
-export default function Header() {
-  // TODO
-}
+import Image from 'next/image';
+import Link from 'next/link';
+
+import commonStyles from '../../styles/common.module.scss';
+import styles from './header.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={commonStyles.container}>
+      <div className={styles.logo}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/images/logo.svg"
+              alt="logo"
+              width={238.62}
+              height={25.63}
+            />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
