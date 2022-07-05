acc87c7b69a67b7215b6657ee0a3ba66ee475914
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-04 22:36:11 -0300

    Implements the page layout and styles

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,9 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import { RichText } from 'prismic-dom';
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -29,6 +32,17 @@ interface PostProps {
 }
 
 const Post: NextPage<PostProps> = ({ post }) => {
+  const formattedPost = {
+    ...post,
+    first_publication_date: format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+  };
+
   return (
     <>
       <Head>
@@ -36,7 +50,39 @@ const Post: NextPage<PostProps> = ({ post }) => {
       </Head>
 
       <main>
-        <h1>{post.data.title}</h1>
+        <img className={styles.banner} src={post.data.banner.url} alt="" />
+
+        <article className={commonStyles.container}>
+          <h1 className={styles.heading}>{formattedPost.data.title}</h1>
+
+          <div className={styles.postDetails}>
+            <div>
+              <FiCalendar />
+              <time>{formattedPost.first_publication_date}</time>
+            </div>
+            <div>
+              <FiUser />
+              <span>{formattedPost.first_publication_date}</span>
+            </div>
+
+            <div>
+              <FiClock />
+              <span>4 min</span>
+            </div>
+          </div>
+
+          <div className={styles.content}>
+            {formattedPost.data.content.map(content => (
+              <div>
+                <h3>{content.heading}</h3>
+
+                {content.body.map(paragraph => (
+                  <p>{paragraph.text}</p>
+                ))}
+              </div>
+            ))}
+          </div>
+        </article>
       </main>
     </>
   );
@@ -56,7 +102,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: 'blocking',
+    fallback: true,
   };
 };
 
