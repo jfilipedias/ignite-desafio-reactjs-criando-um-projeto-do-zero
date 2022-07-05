c8b67420e82667094f6008c78260ebd910b2a2b5
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-05 19:55:22 -0300

    Implements page time reading and suspend on fallback

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,19 +1,23 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-param-reassign */
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-import { RichText } from 'prismic-dom';
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
 interface Post {
+  uid: string;
   first_publication_date: string | null;
   data: {
     title: string;
+    subtitle: string;
     banner: {
       url: string;
     };
@@ -32,16 +36,29 @@ interface PostProps {
 }
 
 const Post: NextPage<PostProps> = ({ post }) => {
-  const formattedPost = {
-    ...post,
-    first_publication_date: format(
-      new Date(post.first_publication_date),
-      'dd MMM yyyy',
-      {
-        locale: ptBR,
-      }
-    ),
-  };
+  const formattedDate = format(
+    new Date(post.first_publication_date),
+    'dd MMM yyyy',
+    {
+      locale: ptBR,
+    }
+  );
+
+  const wordCount = post.data.content.reduce((acc, content) => {
+    acc += content.heading.split(' ').length;
+    acc += content.body.reduce((accWords, body) => {
+      accWords += body.text.split(' ').length;
+      return accWords;
+    }, 0);
+
+    return acc;
+  }, 0);
+
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Carregando...</h1>;
+  }
 
   return (
     <>
@@ -53,31 +70,31 @@ const Post: NextPage<PostProps> = ({ post }) => {
         <img className={styles.banner} src={post.data.banner.url} alt="" />
 
         <article className={commonStyles.container}>
-          <h1 className={styles.heading}>{formattedPost.data.title}</h1>
+          <h1 className={styles.heading}>{post.data.title}</h1>
 
           <div className={styles.postDetails}>
             <div>
               <FiCalendar />
-              <time>{formattedPost.first_publication_date}</time>
+              <time>{formattedDate}</time>
             </div>
             <div>
               <FiUser />
-              <span>{formattedPost.first_publication_date}</span>
+              <span>{post.data.author}</span>
             </div>
 
             <div>
               <FiClock />
-              <span>4 min</span>
+              <span>{Math.ceil(wordCount / 200)} min</span>
             </div>
           </div>
 
           <div className={styles.content}>
-            {formattedPost.data.content.map(content => (
-              <div>
+            {post.data.content.map((content, headingIndex) => (
+              <div key={headingIndex}>
                 <h3>{content.heading}</h3>
 
-                {content.body.map(paragraph => (
-                  <p>{paragraph.text}</p>
+                {content.body.map((paragraph, paragraphIndex) => (
+                  <p key={paragraphIndex}>{paragraph.text}</p>
                 ))}
               </div>
             ))}
@@ -113,9 +130,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const response = await prismic.getByUID('post', String(slug), {});
 
   const post = {
+    uid: response.uid,
     first_publication_date: response.first_publication_date,
     data: {
       title: response.data.title,
+      subtitle: response.data.subtitle,
       banner: {
         url: response.data.banner.url,
       },
